8d3450a3e8947a939c79da00d673254a85e05f49
berezin <isbushcar@gmail.com>
2021-04-26 16:16:43 +1000

    test fix

--- tests/test.py
+++ tests/test.py
@@ -1,22 +1,33 @@
 import os.path
-
+import requests
 import tempfile
 
 from page_loader import download
 
 
-CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
-
-
-def file_download():
-    hexlet_courses = 'https://ru.hexlet.io/courses'
+def test_page_download():
+    hexlet_courses = 'https://ru.hexlet.io/courses/'
     with tempfile.TemporaryDirectory() as tmpdirname:
-        download(hexlet_courses, tmpdirname)
-        result_file = os.path.join(tmpdirname, 'ru-hexlet-io-courses.html')
+        directory = os.path.join(tmpdirname, 'test')
+        download(hexlet_courses, directory)
+        result_file = os.path.join(directory, 'ru-hexlet-io-courses.html')
         assert os.path.exists(result_file) is True
-        expected_content = os.path.join(CURRENT_DIR, 'fixtures/hexlet_courses.html')
-        assert open(result_file).read() == open(expected_content).read()
+        file_content = delete_csrf_token(open(result_file).read())
+        expected_content = requests.get('https://ru.hexlet.io/courses')
+        expected_content.raise_for_status()
+        expected_content = delete_csrf_token(expected_content.text)
+        assert file_content == expected_content
+
+
+def delete_csrf_token(page_content):
+    token_position = page_content.find('<meta name="csrf-token" content=') + (
+        len('<meta name="csrf-token" content="')
+    )
+    if token_position == -1:
+        return page_content
+    token_end_position = page_content.find('" />', token_position)
+    return page_content[:token_position] + page_content[token_end_position]
 
 
 if __name__ == '__main__':
-    file_download()
+    test_page_download()

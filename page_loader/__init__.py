ebbba3b2aeb9b0ff5c8901253e89fc9de01e557c
berezin <isbushcar@gmail.com>
2021-04-26 16:17:22 +1000

    page_loader stage_1 version

--- page_loader/__init__.py
+++ page_loader/__init__.py
@@ -1,1 +1,3 @@
-from page_loader.page_loader import download
+# noqa: D103, D104
+
+from page_loader.page_loader import download  # noqa: F401
--- page_loader/page_loader.py
+++ page_loader/page_loader.py
@@ -1,2 +1,23 @@
+"""Contain main downloader module."""
+
+
+import os
+
+import requests
+
+
 def download(url, dir_to_save):
-    pass
+    """Save internet page to specified directory."""
+    file_name = get_file_name(url)
+    if not os.path.exists(os.path.join(os.getcwd(), dir_to_save)):
+        os.mkdir(os.path.join(os.getcwd(), dir_to_save))
+    file_to_save = open(os.path.join(os.getcwd(), dir_to_save, file_name), 'w')  # noqa: WPS515, E501
+    file_to_save.write(requests.get(url).text)
+
+
+def get_file_name(url):
+    """Return file name that depends on page's url."""
+    no_scheme_url = url[url.find(':') + 3:]
+    file_name = '-'.join(no_scheme_url.split('.'))
+    file_name = '-'.join(file_name.split('/'))
+    return f'{file_name.strip("-")}.html'  # noqa" WPS237

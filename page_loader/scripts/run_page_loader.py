325a302da851821f6dadc2a14bb4c506cc609fd9
berezin <isbushcar@gmail.com>
2021-05-13 17:28:32 +1000

    Revert "sys.exit code changed to 0"
    This reverts commit e61625b9a38d45b9229f297e82c50ed15a8c157b.

--- page_loader/scripts/run_page_loader.py
+++ page_loader/scripts/run_page_loader.py
@@ -24,18 +24,23 @@ def parse_args():
 def main():  # noqa: C901
     """Run page_loader.download with parsed arguments."""
     args = parse_args()
+    error_message = ''
+    logging_offer = "\nFor more details please use '--logging' flag and check page-loader.log"  # noqa: E501
     if args.logging:
         logging.basicConfig(filename='page-loader.log', level=logging.DEBUG)
-    try:
+    try:  # noqa: WPS225
         download(args.page_url, args.directory_to_save)
-    except (requests.ConnectionError,
-            TimeoutError,
-            requests.HTTPError,
-            NotADirectoryError,
-            PermissionError,
-            ) as error:
-        logging.error(error)
-        sys.exit(0)
+    except (requests.ConnectionError, TimeoutError):
+        error_message = 'Error while connecting. Please check URI and your internet connection.'  # noqa: E501
+    except requests.HTTPError:
+        error_message = 'HTTPError while getting content.'
+    except NotADirectoryError as error:
+        error_message = f'{error}'
+    except PermissionError:
+        error_message = "Permission error: can't write file to destination directory."  # noqa: E501
+    finally:
+        if error_message:
+            sys.exit(f'{error_message}{logging_offer}')
 
 
 if __name__ == '__main__':
